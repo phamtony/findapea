b4f328b3ffeb9d41fe3f102eb82ad56220d5378b
Tony Pham <tony.p@lashowroom.com>
2016-07-13 16:21:35 -0700

    iteration styling homepage

--- index.php
+++ index.php
@@ -32,29 +32,27 @@
 						<div class="findapea-index-hero">
 							<div class="findapea-index-hero-container">
 
-								<!-- Video Background -->
-								<div class="video-bg">
-									<div class="video-bg-container">
-										<video autoplay loop="loop" poster="https://d35wvr8lexh22u.cloudfront.net/videos/homepage/chicago/video_home_chi_west_2016.jpg">
-										  <source src="https://a0.muscache.com/airbnb/static/P1-background-vid-compressed-2.mp4" type="video/mp4">
-										  <source src="https://a0.muscache.com/airbnb/static/P1-background-vid-compressed-2.webm" type="video/webm">
-										</video>
+								<!-- Image Background -->
+								<div class="image-bg">
+									<div class="image-bg-container">
+										<img src="/img/hero copy.jpg" alt="">
 									</div>
 								</div>
 
 								<!-- Home Header -->
 								<div class="index-header">
-									<div class="index-header-container clearfix">
+									<div class="index-header-container cs-md-sub-fullpage-pad cs-fullpage-pad cs-both-sub-seperation clearfix">
 										
 										<div class="index-header-left-side pull-xs-left">
 											<div class="index-header-left-side-container">
-												<h1><i class="fa fa-h-square" aria-hidden="true">ometel</i></h1>
+												<h2>Findapea</h2>
 											</div>
 										</div>
 
 										<div class="index-header-right-side pull-xs-right">
 											<div class="index-header-right-side-container">
-												<ul class="list-unstyled clearfix">
+												<i class="fa fa-bars hidden-md-up" aria-hidden="true"></i>
+												<ul class="list-unstyled clearfix hidden-sm-down">
 													<li><a href="#">Sign up</a></li>
 													<li><a href="#">Log In</a></li>
 												</ul>
@@ -64,15 +62,18 @@
 									</div>
 								</div>
 
+
 								<!-- Search bar -->
 								<div class="index-search-bar">
 									<div class="index-search-bar-container center mid-wrap">
 
-										<p class="text-xs-center">Match Made Roommates</p>
+										<p class="cs-sub-seperation index-search-bar-title text-xs-center">You are about to be<br>in good company</p>
+										<p class="cs-sub-seperation text-xs-center hidden-sm-down">A personality base search is a click away</p>
 
 										<div class="search-home-bar">
-											<div class="search-home-bar-container center mid-wrap">
+											<div class="search-home-bar-container text-xs-center mid-wrap">
 												<input class="center" type="text" placeholder="Find your perfect roommate">
+												<button>Search</button>
 											</div>
 										</div>
 
