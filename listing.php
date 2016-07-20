47fa551a752cddb95cc631bcf69120183e7dcea1
Tony Pham <mrphamtony@gmail.com>
2016-07-20 00:18:59 -0700

    listing page start

---
+++ listing.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Findapea - Listing Page</title>
+
+	<?php include('_head.css.inc.php');?>
+
+</head>
+
+<body>
+
+
+	<header>
+		<?php include('_header.inc.php');?>		
+	</header>
+
+
+	<main>
+		<div class="findapea-listing-page">
+			<div class="findapea-listing-page-container">
+
+	
+				<div class="container-fluid">
+
+
+
+
+
+
+				</div>		
+
+
+
+			</div>
+		</div>		
+	</main>
+
+	<footer>
+		<?php include('_footer.inc.php');?>
+	</footer>
+
+	<?php include('_js.inc.php');?>
+
+
+</body>
+</html>				
