124f06c1064e196a76fc5d0c0943245a3d27a7b7
Tony Pham <mrphamtony@gmail.com>
2016-08-14 00:52:34 -0700

    header solidify

--- _header.inc.php
+++ _header.inc.php
@@ -2,25 +2,54 @@
 <div class="findapea-header cs-md-sub-fullpage-pad cs-fullpage-pad">
 	<div class="findapea-header-container clearfix">
 		
-		<div class="findapea-header-left-side pull-xs-left">
-			<div class="findapea-header-left-side-container">
+		<div class="findapea-header-logo-side pull-xs-left">
+			<div class="findapea-header-logo-side-container">
 				<a href="/">
 					<img src="/img/findapea_logo.svg" alt="">
 				</a>
 			</div>
 		</div>
 
-		<div class="findapea-header-right-side cs-both-sub-seperation pull-xs-right">
-			<div class="findapea-header-right-side-container">
-				<i class="fa fa-bars hidden-md-up" aria-hidden="true"></i>
-				<ul class="list-unstyled clearfix hidden-sm-down">
-<!-- 					<li><i class="fa fa-lock" aria-hidden="true"></i> <a href="/registration.php"><span>Sign up / </span></a><a href="/login.php"><span>Log In</span></a></li> -->
-					<li><a href="/message.php"><span>Messages</span> <i class="fa fa-commenting-o" aria-hidden="true"></i></a></li>
-					<li><a href="/help.php"><span>Help</span> <i class="fa fa-question-circle" aria-hidden="true"></i></a></li>
-					<li><a href="/account.php"><span>Tony</span> <i class="fa fa-user" aria-hidden="true"></i></a></li>
-				</ul>
-			</div>
-		</div>										
+
+
+
+	<div class="findapea-header-list-menu">
+		<div class="findapea-header-list-menu-container">
+			
+			
+
+			<ul class="clearfix" id="menu">
+
+				<li class="findapea-header-left-menu pull-xs-left">
+					<ul class="list-unstyled clearfix hidden-lg-down">
+						<li class="header-left-selection"><span>Roommate Search</span></li>
+						<li><span>Listing Search</span></li>
+						<li><span>Forums</span></li>
+						<li><span>Create Listing</span></li>
+					</ul>					
+				</li>
+
+
+				<li class="findapea-header-right-side cs-both-sub-seperation pull-xs-right">
+					<ul class="list-unstyled clearfix hidden-lg-down">
+	<!-- 					<li><i class="fa fa-lock" aria-hidden="true"></i> <a href="/registration.php"><span>Sign up / </span></a><a href="/login.php"><span>Log In</span></a></li> -->
+						<li><a href="/message.php"><i class="fa fa-commenting-o" aria-hidden="true"></i><span>Messages</span></a></li>
+						<li><a href="/help.php"><i class="fa fa-question-circle" aria-hidden="true"></i><span>Help</span></a></li>
+						<li><a href="/account.php"><i class="fa fa-user" aria-hidden="true"></i><span>Tony</span></a></li>
+					</ul>					
+				</li>
+
+
+				<li class="pull-xs-right cs-both-sub-seperation">
+					<i class="fa fa-bars hidden-xl-up" aria-hidden="true"></i>
+				</li>				
+
+
+			</ul>
+
+		</div>
+	</div>
+
 
 	</div>
 </div>
